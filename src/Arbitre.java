139fd66908b8ef1b3eaf5f7ace6b03a823ac057f
rezatera854 <m.aubry130@gmail.com>
2019-03-17 16:22:02 +0100

    hallla est mon frere

--- src/Arbitre.java
+++ src/Arbitre.java
@@ -1,4 +1,12 @@
 public class Arbitre extends Personne{
     private int num_match;
     private String position;
+
+    public String getPosition(String position) {
+        return position;
+    }
+    
+    public String getnum_match(int num_match) {
+        return position;
+    }
 }
