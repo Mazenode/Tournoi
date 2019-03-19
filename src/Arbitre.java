0edd622c27e56db4d716bf6729377a9a9c73e0ef
Mathieu Aubry <m.aubry130@gmail.com>
2019-03-19 17:01:40 +0100

    ajout de get....() pour les class Arbitre et Competition

--- src/Arbitre.java
+++ src/Arbitre.java
@@ -2,12 +2,17 @@ public class Arbitre extends Personne{
     private int num_match;
     private String position;
 
+    public Arbitre(int vnum_match, String vposition){
+      num_match = vnum_match;
+      position = vposition;
+    }
+
     public String getPosition(String position) {
         return position;
     }
 
     public String getnum_match(int num_match) {
         return num_match;
-        System.out.println("salut");
+
     }
 }
--- src/Competition.java
+++ src/Competition.java
@@ -2,4 +2,19 @@ public class Competition extends Equipe{
     private String nom_compet;
     private int nbr_eq;
     private String lieu;
+
+    public Competition(String vnom_compet, int vnbr_eq, String vlieu){
+      vnom_compet = nom_compet;
+      vnbr_eq = nbr_eq;
+      vlieu = lieu;
+    }
+    public String getnom_compet(){
+      return(nom_compet);
+    }
+    public int getnbr_eq(){
+      return(nbr_eq);
+    }
+    public String getlieu(){
+      return(lieu);
+    }
 }
